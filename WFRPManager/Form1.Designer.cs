57a9aca6781198d8beba6c1c7068462e11afaa13
Senti <wojciech.warwas01@gmail.com>
2020-07-22 19:21:36 +0200

    Add advanced UI and UI control.

--- WFRPManager/Form1.Designer.cs
+++ WFRPManager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace WFRPManager
 {
-    partial class Form1
+    partial class MainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -33,13 +33,15 @@
             this.BottomStatusStrip = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
             this.MainPanel = new System.Windows.Forms.Panel();
-            this.LeftPictureBox = new System.Windows.Forms.PictureBox();
-            this.RightPictureBox = new System.Windows.Forms.PictureBox();
+            this.FirstPictureBox = new System.Windows.Forms.PictureBox();
+            this.SecondPictureBox = new System.Windows.Forms.PictureBox();
+            this.LeftFlowPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.NextPage = new System.Windows.Forms.Button();
             this.TopMenuStrip.SuspendLayout();
             this.BottomStatusStrip.SuspendLayout();
             this.MainPanel.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.LeftPictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.RightPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FirstPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // TopMenuStrip
@@ -48,7 +50,7 @@
             this.placeholderToolStripMenuItem});
             this.TopMenuStrip.Location = new System.Drawing.Point(0, 0);
             this.TopMenuStrip.Name = "TopMenuStrip";
-            this.TopMenuStrip.Size = new System.Drawing.Size(783, 24);
+            this.TopMenuStrip.Size = new System.Drawing.Size(1027, 24);
             this.TopMenuStrip.TabIndex = 0;
             this.TopMenuStrip.Text = "TopMenuStrip";
             // 
@@ -62,9 +64,9 @@
             // 
             this.BottomStatusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.toolStripStatusLabel1});
-            this.BottomStatusStrip.Location = new System.Drawing.Point(0, 480);
+            this.BottomStatusStrip.Location = new System.Drawing.Point(0, 464);
             this.BottomStatusStrip.Name = "BottomStatusStrip";
-            this.BottomStatusStrip.Size = new System.Drawing.Size(783, 22);
+            this.BottomStatusStrip.Size = new System.Drawing.Size(1027, 22);
             this.BottomStatusStrip.TabIndex = 1;
             this.BottomStatusStrip.Text = "BottomStatusStrip";
             // 
@@ -77,55 +79,75 @@
             // MainPanel
             // 
             this.MainPanel.AutoScroll = true;
-            this.MainPanel.Controls.Add(this.RightPictureBox);
-            this.MainPanel.Controls.Add(this.LeftPictureBox);
+            this.MainPanel.Controls.Add(this.NextPage);
+            this.MainPanel.Controls.Add(this.LeftFlowPanel);
+            this.MainPanel.Controls.Add(this.FirstPictureBox);
+            this.MainPanel.Controls.Add(this.SecondPictureBox);
             this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.MainPanel.Location = new System.Drawing.Point(0, 24);
             this.MainPanel.Name = "MainPanel";
-            this.MainPanel.Size = new System.Drawing.Size(783, 456);
+            this.MainPanel.Size = new System.Drawing.Size(1027, 440);
             this.MainPanel.TabIndex = 2;
             // 
-            // LeftPictureBox
+            // FirstPictureBox
             // 
-            this.LeftPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
-            this.LeftPictureBox.ImageLocation = "D:\\vs\\repos\\WFRPManager\\WFRPManager\\resources\\w0.png";
-            this.LeftPictureBox.Location = new System.Drawing.Point(0, 0);
-            this.LeftPictureBox.Name = "LeftPictureBox";
-            this.LeftPictureBox.Size = new System.Drawing.Size(1675, 439);
-            this.LeftPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.LeftPictureBox.TabIndex = 0;
-            this.LeftPictureBox.TabStop = false;
+            this.FirstPictureBox.ImageLocation = "D:\\vs\\repos\\WFRPManager\\WFRPManager\\resources\\w0.png";
+            this.FirstPictureBox.Location = new System.Drawing.Point(193, 0);
+            this.FirstPictureBox.Name = "FirstPictureBox";
+            this.FirstPictureBox.Size = new System.Drawing.Size(1675, 2175);
+            this.FirstPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.FirstPictureBox.TabIndex = 0;
+            this.FirstPictureBox.TabStop = false;
             // 
-            // RightPictureBox
+            // SecondPictureBox
             // 
-            this.RightPictureBox.Dock = System.Windows.Forms.DockStyle.Left;
-            this.RightPictureBox.ImageLocation = "D:\\vs\\repos\\WFRPManager\\WFRPManager\\resources\\w1.png";
-            this.RightPictureBox.Location = new System.Drawing.Point(1675, 0);
-            this.RightPictureBox.Name = "RightPictureBox";
-            this.RightPictureBox.Size = new System.Drawing.Size(1675, 439);
-            this.RightPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
-            this.RightPictureBox.TabIndex = 1;
-            this.RightPictureBox.TabStop = false;
+            this.SecondPictureBox.ImageLocation = "D:\\vs\\repos\\WFRPManager\\WFRPManager\\resources\\w1.png";
+            this.SecondPictureBox.Location = new System.Drawing.Point(193, 0);
+            this.SecondPictureBox.Name = "SecondPictureBox";
+            this.SecondPictureBox.Size = new System.Drawing.Size(1675, 2175);
+            this.SecondPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.SecondPictureBox.TabIndex = 1;
+            this.SecondPictureBox.TabStop = false;
             // 
-            // Form1
+            // LeftFlowPanel
+            // 
+            this.LeftFlowPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LeftFlowPanel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftFlowPanel.Location = new System.Drawing.Point(0, 0);
+            this.LeftFlowPanel.Name = "LeftFlowPanel";
+            this.LeftFlowPanel.Size = new System.Drawing.Size(200, 2175);
+            this.LeftFlowPanel.TabIndex = 2;
+            // 
+            // NextPage
+            // 
+            this.NextPage.Location = new System.Drawing.Point(1834, 3);
+            this.NextPage.Name = "NextPage";
+            this.NextPage.Size = new System.Drawing.Size(32, 32);
+            this.NextPage.TabIndex = 3;
+            this.NextPage.Text = ">";
+            this.NextPage.UseVisualStyleBackColor = true;
+            this.NextPage.Click += new System.EventHandler(this.NextPage_Click);
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 502);
+            this.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.ClientSize = new System.Drawing.Size(1027, 486);
             this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.BottomStatusStrip);
             this.Controls.Add(this.TopMenuStrip);
             this.MainMenuStrip = this.TopMenuStrip;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainWindow";
+            this.Text = "WFRPManager";
             this.TopMenuStrip.ResumeLayout(false);
             this.TopMenuStrip.PerformLayout();
             this.BottomStatusStrip.ResumeLayout(false);
             this.BottomStatusStrip.PerformLayout();
             this.MainPanel.ResumeLayout(false);
             this.MainPanel.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.LeftPictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.RightPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.FirstPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SecondPictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -138,8 +160,10 @@
         private System.Windows.Forms.StatusStrip BottomStatusStrip;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
         private System.Windows.Forms.Panel MainPanel;
-        private System.Windows.Forms.PictureBox LeftPictureBox;
-        private System.Windows.Forms.PictureBox RightPictureBox;
+        private System.Windows.Forms.PictureBox FirstPictureBox;
+        private System.Windows.Forms.PictureBox SecondPictureBox;
+        private System.Windows.Forms.FlowLayoutPanel LeftFlowPanel;
+        private System.Windows.Forms.Button NextPage;
     }
 }
 
