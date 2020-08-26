377afcdfd127228b8cb72dd781c148dfd247c93b
Senti <wojciech.warwas01@gmail.com>
2020-08-26 10:35:21 +0200

    Add basic login validation

--- WFRPManager/LoginRegistration.Designer.cs
+++ WFRPManager/LoginRegistration.Designer.cs
@@ -30,7 +30,12 @@
         {
             this.TopPanel = new System.Windows.Forms.Panel();
             this.LoginLable = new System.Windows.Forms.Label();
+            this.MainPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.Login = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.LoginButton = new System.Windows.Forms.Button();
             this.TopPanel.SuspendLayout();
+            this.MainPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // TopPanel
@@ -40,7 +45,7 @@
             this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
             this.TopPanel.Location = new System.Drawing.Point(0, 0);
             this.TopPanel.Name = "TopPanel";
-            this.TopPanel.Size = new System.Drawing.Size(496, 100);
+            this.TopPanel.Size = new System.Drawing.Size(273, 100);
             this.TopPanel.TabIndex = 0;
             // 
             // LoginLable
@@ -50,22 +55,69 @@
             this.LoginLable.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
             this.LoginLable.Location = new System.Drawing.Point(0, 0);
             this.LoginLable.Name = "LoginLable";
-            this.LoginLable.Size = new System.Drawing.Size(496, 100);
+            this.LoginLable.Size = new System.Drawing.Size(273, 100);
             this.LoginLable.TabIndex = 0;
             this.LoginLable.Text = "Zaloguj się";
             this.LoginLable.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainPanel
+            // 
+            this.MainPanel.Controls.Add(this.Login);
+            this.MainPanel.Controls.Add(this.Password);
+            this.MainPanel.Controls.Add(this.LoginButton);
+            this.MainPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPanel.Location = new System.Drawing.Point(0, 100);
+            this.MainPanel.Name = "MainPanel";
+            this.MainPanel.Padding = new System.Windows.Forms.Padding(32, 24, 32, 24);
+            this.MainPanel.Size = new System.Drawing.Size(273, 355);
+            this.MainPanel.TabIndex = 1;
+            // 
+            // Login
+            // 
+            this.Login.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Login.Location = new System.Drawing.Point(32, 24);
+            this.Login.Margin = new System.Windows.Forms.Padding(0, 0, 0, 32);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(206, 20);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "LOGIN";
+            // 
+            // Password
+            // 
+            this.Password.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Password.Location = new System.Drawing.Point(32, 56);
+            this.Password.Margin = new System.Windows.Forms.Padding(0, 0, 0, 32);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(206, 20);
+            this.Password.TabIndex = 1;
+            this.Password.Text = "PASSWORD";
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(96, 88);
+            this.LoginButton.Margin = new System.Windows.Forms.Padding(64, 0, 0, 0);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 2;
+            this.LoginButton.Text = "Login";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
             // 
             // LoginRegistration
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(496, 473);
+            this.ClientSize = new System.Drawing.Size(273, 455);
+            this.Controls.Add(this.MainPanel);
             this.Controls.Add(this.TopPanel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "LoginRegistration";
             this.Text = "WFRPManager";
             this.TopPanel.ResumeLayout(false);
+            this.MainPanel.ResumeLayout(false);
+            this.MainPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -74,5 +126,9 @@
 
         private System.Windows.Forms.Panel TopPanel;
         private System.Windows.Forms.Label LoginLable;
+        private System.Windows.Forms.FlowLayoutPanel MainPanel;
+        private System.Windows.Forms.TextBox Login;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button LoginButton;
     }
 }
--- WFRPManager/LoginRegistration.cs
+++ WFRPManager/LoginRegistration.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,14 @@ namespace WFRPManager
         public LoginRegistration()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Login.Text) && !string.IsNullOrEmpty(Password.Text) && Regex.IsMatch(Login.Text, @"^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$"))
+                MessageBox.Show(":)");
+            else
+                MessageBox.Show(":(");
         }
     }
 }
